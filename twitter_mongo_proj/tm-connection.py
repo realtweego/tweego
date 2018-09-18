d791ea87f1efca22bbb01708c2471c63db67bb1b
ktroutman <kent.troutman@gmail.com>
2018-09-18 11:26:56 +0200

    added cloud db code

--- twitter_mongo_proj/tm-connection.py
+++ twitter_mongo_proj/tm-connection.py
@@ -1,5 +1,8 @@
 
 import argparse
+import pymongo
+import urllib
+
 from twitter_mongo_proj.scrape_twitter import get_tweets
 from twitter_mongo_proj.load_mongo import load_data
 
@@ -7,25 +10,29 @@ from twitter_mongo_proj.load_mongo import load_data
 #1) Define Database
 MONGO_HOST = 'mongodb://localhost/twitter.db'
 
-
+"""
+username = urllib.parse.quote_plus('basil_master')
+password = urllib.parse.quote_plus('basil_spice')
+client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@basilcluster-eoh28.mongodb.net/test?retryWrites=true")
+"""
 def run()
     ''' This function uses scrape_twitter.py to access Twitter and retrieve tweets through
         tweepy. It than opens an MongoDB and stores the tweets'''
-            
-    #2) Get tweets based on generator 
-    
+
+    #2) Get tweets based on generator
+
     #2.1) Getting JSON-file of tweets in current working directory
     get_tweets()
-    
-    #2.2) Open tweets-file and store all tweets in a list 
+
+    #2.2) Open tweets-file and store all tweets in a list
     with open('tweets.json') as data:
 
         data_list = []
-   
+
         for i in data:
-       
+
             data_list.append(json.loads(i))
-    
+
     #3) Open Database and store tweets in MongoDB from a JSON-file in current working directory
     load_data(MONGO_HOST, data_list)
 
