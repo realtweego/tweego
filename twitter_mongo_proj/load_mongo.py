77fc2a9265b75b78c8d4582d413baa7bd9188cd6
Azima1985 <mohammada.hammad@gmail.com>
2018-09-18 19:04:42 +0200

    Some cleaning up

--- twitter_mongo_proj/load_mongo.py
+++ twitter_mongo_proj/load_mongo.py
@@ -1,12 +1,12 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 
-def load_data(MONGO_HOST, data_list):
-    client = MONGO_HOST
+def load_data(client, data_list):
+    #client = MONGO_HOST
             
     #insert the data into the mongoDB into a collection called tweets
     #if tweets doesn't exist, it will be created.
     client.twitter.collections.tweets.insert(data_list)
  
 if __name__ == "__main__":
-	load_data(MONGO_HOST, data_list)
+	load_data(client, data_list)
--- twitter_mongo_proj/tm-connection.py
+++ twitter_mongo_proj/tm-connection.py
@@ -2,7 +2,7 @@
 import pymongo
 import urllib
 import json
-from scrape_twitter import get_tweets
+#from scrape_twitter import get_tweets
 from load_mongo import load_data
 from scrape_twitter import get_tweets
 #from pymongo import MongoClient
@@ -11,26 +11,31 @@ username = ""
 password = ""
 username = urllib.parse.quote_plus(username)
 password = urllib.parse.quote_plus(password)
-MONGO_HOST = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@basilcluster-eoh28.mongodb.net/test?retryWrites=true")
-
+#MONGO_HOST = pymongo.MongoClient()
+#f"mongodb+srv://{username}:{password}@basilcluster-eoh28.mongodb.net/test?retryWrites=true"
 
 class read_into_Mongo:
-    def __init__(self,chunk_size,db):
+    def __init__(self,chunk_size,db,limit):
         self.chunk_size = chunk_size
         self.db = db
         self.buffer = []
-
+        self.limit=limit
+        self.counter=0
     def new_tweet(self, text):
         #db = MONGO_HOST
         self.buffer.append(text)
+        if self.limit - self.counter < self.chunk_size:
+            self.chunk_size = self.limit - self.counter
         if len(self.buffer) >= self.chunk_size:
             load_data(self.db,self.buffer)
             self.buffer = []
+            self.counter+=self.chunk_size
         print("Processed Tweet")
 
 
-m = read_into_Mongo(3,MONGO_HOST)
+def load_to_mongo(chunk_size,db,limit):
+    get_tweets(limit,read_into_Mongo(chunk_size,db,limit).new_tweet)
 
-
-# figure out a smarter way to specify this
-get_tweets(9,m.new_tweet)
+if __name__=='__main__':
+    load_to_mongo(3,pymongo.MongoClient(),10)
+    
