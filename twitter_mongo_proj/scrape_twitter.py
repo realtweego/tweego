32c198fb56b4531f0de1610eeb91c1de98cc4491
Azima1985 <mohammada.hammad@gmail.com>
2018-09-18 15:38:46 +0200

    set the limit and callback when you call get_tweets

--- twitter_mongo_proj/scrape_twitter.py
+++ twitter_mongo_proj/scrape_twitter.py
@@ -17,10 +17,15 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 user = api.me()
 print (user.name)
-counter=0
 FILENAME = 'tweets.json'
 
 class StreamListener(tweepy.StreamListener):
+    def __init__(self,limit,callback):
+        super().__init__()
+        self.limit=limit
+        self.counter=0
+        self.callback=callback
+        
     def on_status(self, status):
         print(status.text)
     
@@ -30,7 +35,6 @@ class StreamListener(tweepy.StreamListener):
     
     def on_data(self, data):
     #collect, filter and parse the tweets from twitter API
-        global counter
         t=json.loads(data)
         created_at=t['created_at']
         tweet_id=t['id_str']
@@ -40,28 +44,35 @@ class StreamListener(tweepy.StreamListener):
         user_favorites_count=t['user']['favourites_count']
         retweet_count=t['retweet_count']
         favorite_count=t['favorite_count']
-        if 'retweeted_status' in t and len(t['retweeted_status']) > 0:
-            hashtags=[item['text'] for item in t['retweeted_status']['entities']['hashtags']]
+        if 'hashtags' in t['entities'] and len(t['entities']['hashtags']) > 0:
+            hashtags=[item['text'] for item in t['entities']['hashtags']]
         else:
             hashtags=[]
+        #still working on this
+        '''extended=t.get('extended_tweet','default')
+        if extended!='default':
+            media_url=[]
+            for dic in extended['media']:
+                media_url.append(dic['expanded_url'])
+        else:'''
+        media_url = None
         if t['retweeted'] == False and 'RT' not in t['text']:
             tweet = {'created_at':created_at,'id':tweet_id, 
                      'text':text, 'username':username, 
                      'followers':followers_count, 
                      'user_favorites_count':user_favorites_count, 
                      'retweets':retweet_count,'favorites':favorite_count,
-                     'hashtags':hashtags}
-            with open('tweets.json', 'a') as outfile:
-                    json.dump(tweet, outfile)
-                    outfile.write('\n')
-                    counter +=1
-            if counter==100:
-                    sys.exit(0)
+                     'hashtags':hashtags,'media_url':media_url}
+            self.callback(tweet)
+            self.counter +=1
+            if self.counter==self.limit:
+                    return False
 
-def get_tweets():
-        stream_listener = StreamListener()
+def get_tweets(limit,callback):
+        stream_listener = StreamListener(limit,callback)
         stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
         stream.filter(track=['Machine learning','#ML','BigData','Artificial Intelligence','Big Data'], languages=['en'])#,async=True)
-
+        
+                             
 if __name__ == '__main__':
-    get_tweets()
+    get_tweets(5,print)
