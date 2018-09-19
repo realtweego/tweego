0fc5400ec587b5188a0e9c7eaa857de99d772fda
ErikMann <erik_mann@web.de>
2018-09-19 15:06:15 +0200

    pylint changes
    small formal changes

--- twitter_mongo_proj/tm_connection.py
+++ twitter_mongo_proj/tm_connection.py
@@ -1,26 +1,23 @@
 #import argparse
-import pymongo
 import urllib
-import json
-#from scrape_twitter import get_tweets
+import pymongo
 from load_mongo import load_data
 from scrape_twitter import get_tweets
-#from pymongo import MongoClient
 
 username = ""
 password = ""
 username = ''#urllib.parse.quote_plus(username)
 password = ''#urllib.parse.quote_plus(password)
 #MONGO_HOST = pymongo.MongoClient()
-#f"mongodb+srv://{username}:{password}@basilcluster-eoh28.mongodb.net/test?retryWrites=true"
+#f"mongodb+srv://{username}:{password}@basilcluster-eoh28.mongodb.net/test?retryWrites = true"
 
 class read_into_Mongo:
     def __init__(self,chunk_size,db,limit):
         self.chunk_size = chunk_size
         self.db = db
         self.buffer = []
-        self.limit=limit
-        self.counter=0
+        self.limit = limit
+        self.counter = 0
     def new_tweet(self, text):
         #db = MONGO_HOST
         self.buffer.append(text)
@@ -29,7 +26,7 @@ class read_into_Mongo:
         if len(self.buffer) >= self.chunk_size:
             load_data(self.db,self.buffer)
             self.buffer = []
-            self.counter+=self.chunk_size
+            self.counter += self.chunk_size
         print("Processed Tweet")
 
 
