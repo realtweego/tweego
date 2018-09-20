3a3890ba831d96f61623e9931970c9071729a0a9
ErikMann <erik_mann@web.de>
2018-09-20 15:57:01 +0200

    Update
    Pylint, Travis, cleanup

--- twitter_mongo_proj/twitter_push.py
+++ twitter_mongo_proj/twitter_push.py
@@ -1,26 +1,26 @@
+from math import ceil
 import tweepy
 import pymongo
 from tm_connection import load_to_mongo
 from config import cfg, client
-from math import ceil
 
 #Create function for the API configration to use twitter
-def get_api(cfg):
-  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
-  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
-  return tweepy.API(auth)
+def get_api():
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
 
-def main(chunk_size,limit):
-  api = get_api(cfg)
-  # Create tweet and send it to twitter
-  load_to_mongo(chunk_size,limit)
-  streamed=list(client.twitter.collections.tweets_labeled.find({'interesting':1}).sort('$natural',pymongo.DESCENDING).limit(ceil(int(f'{limit}')/int(f'{chunk_size}'))))
-  followers=[t['followers'] for t in streamed]
-  for t in streamed:
-      if t['followers']==max(followers):
-          tweet= 'By @'+t['username']+'\n\n'+t['text']
-  status = api.update_status(status=tweet)
-  
+def main(chunk_size, limit):
+    api = get_api()
+    # Create tweet and send it to twitter
+    load_to_mongo(chunk_size, limit)
+    streamed = list(client.twitter.collections.tweets_labeled.find({'interesting':1}).\
+                    sort('$natural', pymongo.DESCENDING).limit(ceil(int(f'{limit}')/int(f'{chunk_size}'))))
+    followers = [tw['followers'] for tw in streamed]
+    for tweet in streamed:
+        if tweet['followers'] == max(followers):
+            message = 'By @'+tweet['username']+'\n\n'+tweet['text']
+    return api.update_status(status=message)
 
 if __name__ == "__main__":
-  main(3,10)
+    main(3, 10)
