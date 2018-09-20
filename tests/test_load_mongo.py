f7a56e85a003f190f489c3ea6c235645eff67dfb
RobertoMusumeci90 <rbt.musumeci@gmail.com>
2018-09-20 17:42:52 +0200

    test load mongo

--- tests/test_load_mongo.py
+++ tests/test_load_mongo.py
@@ -2,8 +2,8 @@ from tweego.tm_connection import read_into_Mongo
 from tweego.tm_connection import load_to_mongo
 from tweego.config import client
 import json
-def test_load_mongo(tweet):
 
+def test_load_mongo(tweet):
 	rim = read_into_Mongo(1,1)
 	before = client.twitter.collections.tweets_labeled.count()
 	tweet = json.loads(tweet)
